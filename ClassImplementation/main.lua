8b539402fb9d6db6538967962acd104fc457282f
Mohamed28 <mohamed28.lucas@gmail.com>
2020-12-24 22:33:26 -0300

    Update

--- ClassImplementation/main.lua
+++ ClassImplementation/main.lua
@@ -3,7 +3,7 @@ local player = Player:new({x = 0, y = 0}, 32, 32)
 
 function love.load()
     player:setAvatar("boy")
-    player.avatar:addAnimation("idle", 4, {0.8, 0.25, 0.1, 0.25})
+    player.avatar:addAnimation("idle", 4, {2, 0.25, 0.1, 0.25})
     player.avatar:addAnimation("run", 10, 0.8)
     player.avatar:addAnimation("jump", 10, 0.8)
 end
